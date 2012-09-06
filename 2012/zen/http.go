150bb95497089cf8cc765e9aa4cae4c3d26f9c05
Andrew Gerrand <adg@golang.org>
2012-09-06 11:00:15 +1000

    go.talks: add Go and the Zen of Python talk
    R=r, campoy, lucio.dere, dsymonds, iant, mpvl, sameer
    CC=golang-dev
    https://golang.org/cl/6506073

---
+++ 2012/zen/http.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe("localhost:8000", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Pythonistas!")
+}
