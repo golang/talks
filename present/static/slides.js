3ac8d984514b30ceefdabc2a90e41ae927cbc146
Francesc Campoy <campoy@golang.org>
2012-09-11 17:21:37 +1000

    go.talks: fix slides.js to work from HTTPS servers
    R=adg
    CC=golang-dev
    https://golang.org/cl/6501116

--- present/static/slides.js
+++ present/static/slides.js
@@ -504,7 +504,7 @@ function addFontStyle() {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
   el.type = 'text/css';
-  el.href = 'http://fonts.googleapis.com/css?family=' +
+  el.href = '//fonts.googleapis.com/css?family=' +
             'Open+Sans:regular,semibold,italic,italicsemibold|Droid+Sans+Mono';
 
   document.body.appendChild(el);
