0cc7d240e79560dacc3af699a309f7d4dda6d2f4
Rob Pike <r@golang.org>
2012-11-15 10:41:27 -0800

    go.talks/present: document the .extensions Related: remove stale mention of -template flag.
    R=golang-dev, campoy, adg
    CC=golang-dev
    https://golang.org/cl/6851046

--- present/doc.go
+++ present/doc.go
@@ -13,11 +13,15 @@ presentation. The App Engine version does not provide this functionality.
 Usage of present:
   -base="": base path for slide template and static resources
   -http="127.0.0.1:3999": host:port to listen on
-  -template="": alternate slide template file
 
 You may use the app.yaml file provided in the root of the go.talks repository
 to deploy present to App Engine:
 	appcfg.py update -A your-app-id -V your-app-version /path/to/go.talks
+
+Input files are named foo.extension, where "extension" defines the format of
+the generated output. The supported formats are:
+	.slide        // HTML5 slide presentation
+	.article      // article format, such as a blog post
 
 The source file format
 
