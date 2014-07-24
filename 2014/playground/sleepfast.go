6e6b036b90060bd6a1491f77e62c6ef28e77d846
Francesc Campoy <campoy@golang.org>
2014-07-24 09:23:05 -0700

    go.talks: fix typo and adding slide to Inside the playground
    LGTM=crawshaw
    R=adg, crawshaw
    CC=golang-codereviews
    https://golang.org/cl/117150045

---
+++ 2014/playground/sleepfast.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println(start)
+
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Nanosecond)
+		fmt.Println(time.Since(start))
+	}
+}
