90258e814a151df3bffe2f3f164871445f63ce87
David Crawshaw <david.crawshaw@zentus.com>
2014-11-20 11:24:19 -0500

    x/talks/2014: GothamGo talk on Android
    LGTM=adg
    R=adg
    CC=golang-codereviews
    https://golang.org/cl/172400044

---
+++ 2014/gothamgo-android/red.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/app/debug"
+	"golang.org/x/mobile/gl"
+)
+
+func main() {
+	app.Run(app.Callbacks{
+		Draw: draw,
+	})
+}
+
+func draw() {
+	gl.ClearColor(1, 0, 0, 1) // RGBA value used to clear buffer: red
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	debug.DrawFPS()
+}
