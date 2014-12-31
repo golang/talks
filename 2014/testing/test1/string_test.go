7b66936d683f1da7df7de6648cebece18465ab84
Shenghou Ma <minux@golang.org>
2014-12-30 19:01:02 -0500

    2014/testing/test1/string_test.go: fix swapped arguments
    Fixes golang/go#9213.
    
    Change-Id: I58c7da89780baeb1cc7d3811e3d89a4dae406777
    Reviewed-on: https://go-review.googlesource.com/2230
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- 2014/testing/test1/string_test.go
+++ 2014/testing/test1/string_test.go
@@ -9,6 +9,6 @@ func TestIndex(t *testing.T) {
 	const s, sep, want = "chicken", "ken", 4
 	got := strings.Index(s, sep)
 	if got != want {
-		t.Errorf("Index(%q,%q) = %v; want %v", s, sep, want, got)
+		t.Errorf("Index(%q,%q) = %v; want %v", s, sep, got, want)
 	}
 }
